3c2f31a1603f8afd466aaf561bcb217859d1acb5
arthur_wxy <kzzn@qq.com>
2020-09-07 21:32:17 +0800

    测试用

--- playground.py
+++ playground.py
@@ -1,3 +1,15 @@
-registry = []
-def register(func):
-    print('running register')
+def nth_power(a):
+    def exponent_of(b):
+        return b ** a
+    return exponent_of
+
+square = nth_power(2)
+cube = nth_power(3)
+
+res1 = square(2)
+res2 = square(3)
+
+print(square(2))
+
+print(res1)
+print(res2)
